1dda0cd05f71003e6127c7a9e49e674e6f0e9119
Marut Siripan <marut.siripan@okkami.com>
2017-03-14 19:06:13 +0700

    [FD-3499-ANDROID] Add line login and update package name to com.okkami.android.app.

--- android/src/main/java/com/okkami/okkamisdk/OkkamiSdkModule.java
+++ android/src/main/java/com/okkami/okkamisdk/OkkamiSdkModule.java
@@ -1,19 +1,29 @@
 package com.okkami.okkamisdk;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.gson.JsonObject;
+import com.linecorp.linesdk.auth.LineLoginApi;
+import com.linecorp.linesdk.auth.LineLoginResult;
 import com.okkami.android.sdk.SDK;
 import com.okkami.android.sdk.enums.AUTH_TYPE;
 import com.okkami.android.sdk.model.BaseAuthentication;
 import com.okkami.android.sdk.model.CompanyAuth;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
@@ -30,11 +40,97 @@ class OkkamiSdkModule extends ReactContextBaseJavaModule {
     private Context context;
     private static final String TAG = "OKKAMISDK";
     private SDK okkamiSdk;
+    private Promise lineLoginPromise = null;
+
+    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode,
+                Intent data) {
+            super.onActivityResult(activity, requestCode, resultCode, data);
+            //            super.onActivityResult(requestCode, resultCode, data);
+//            if (requestCode != REQUEST_CODE) {
+//                return;
+//            }
+            Log.d(TAG, "onActivityResult: "+requestCode);
+            LineLoginResult result = LineLoginApi.getLoginResultFromIntent(data);
+            String accessToken = result.getLineCredential().getAccessToken().getAccessToken();
+
+            switch (result.getResponseCode()) {
+
+                case SUCCESS:
+                    // Login is successful
+                    // Do something...
+                    JSONObject jObj = new JSONObject();
+                    try {
+                        jObj.put("accessToken", accessToken);
+                        jObj.put("user_id", result.getLineProfile().getUserId());
+                        jObj.put("display_name", result.getLineProfile().getDisplayName());
+                        jObj.put("picture", result.getLineProfile().getPictureUrl().toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        lineLoginPromise.reject("error", e.getMessage());
+                    }
+
+                    lineLoginPromise.resolve(jObj.toString());
+//                    Intent transitionIntent = new Intent(this, PostLoginActivity.class);
+//
+//                    transitionIntent.putExtra("display_name", result.getLineProfile().getDisplayName());
+//                    transitionIntent.putExtra("status_message", result.getLineProfile().getStatusMessage());
+//                    transitionIntent.putExtra("user_id", result.getLineProfile().getUserId());
+//                    transitionIntent.putExtra("picture_url", result.getLineProfile().getPictureUrl().toString());
+                    break;
+                case CANCEL:
+                    // Login was cancelled by the user
+                    // Do something...
+                    lineLoginPromise.reject("error", "error");
+                    break;
+                default:
+                    // Login was cancelled by the user
+                    // Do something...
+            }
+        }
+
+//        @Override
+//        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+////            super.onActivityResult(requestCode, resultCode, data);
+////            if (requestCode != REQUEST_CODE) {
+////                return;
+////            }
+//            Log.d(TAG, "onActivityResult: "+requestCode);
+//            LineLoginResult result = LineLoginApi.getLoginResultFromIntent(data);
+//            String accessToken = result.getLineCredential().getAccessToken().getAccessToken();
+//
+//            switch (result.getResponseCode()) {
+//
+//                case SUCCESS:
+//                    // Login is successful
+//                    // Do something...
+//                    lineLoginPromise.resolve(accessToken);
+////                    Intent transitionIntent = new Intent(this, PostLoginActivity.class);
+////
+////                    transitionIntent.putExtra("display_name", result.getLineProfile().getDisplayName());
+////                    transitionIntent.putExtra("status_message", result.getLineProfile().getStatusMessage());
+////                    transitionIntent.putExtra("user_id", result.getLineProfile().getUserId());
+////                    transitionIntent.putExtra("picture_url", result.getLineProfile().getPictureUrl().toString());
+//                    break;
+//                case CANCEL:
+//                    // Login was cancelled by the user
+//                    // Do something...
+//                    lineLoginPromise.reject("error", "error");
+//                    break;
+//                default:
+//                    // Login was cancelled by the user
+//                    // Do something...
+//            }
+//        }
+    };
 
     public OkkamiSdkModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.context = reactContext;
 
+        reactContext.addActivityEventListener(mActivityEventListener);
         okkamiSdk = new SDK().init(context, "https://api.fingi.com"); // TODO : how do we pass the URL dynamically from react??
     }
 
@@ -49,6 +145,13 @@ class OkkamiSdkModule extends ReactContextBaseJavaModule {
 
 
      /*-------------------------------------- Utility   --------------------------------------------------*/
+
+    @ReactMethod
+    public void lineLogin(Promise lineLoginPromise) {
+        this.lineLoginPromise = lineLoginPromise;
+        Intent loginIntent = LineLoginApi.getLoginIntent(this.context, "1499319131");
+        getCurrentActivity().startActivityForResult(loginIntent, 1);
+    }
 
 
 
