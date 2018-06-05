4ec6eb4b217428b9c50e22494b890c9c98205dd8
Marut Siripan <marut.siripan@okkami.com>
2018-06-05 16:08:27 +0700

    feature/FD-6915 Fixed app crashed in about screen.

--- okkamisdk.js
+++ okkamisdk.js
@@ -1,4 +1,5 @@
 import {NativeModules, DeviceEventEmitter} from 'react-native'
+import Metrics from '../App/Themes/Metrics'
 import moment from 'moment'
 // import {convertRgbaToHex} from '../App/Lib/Utilities'
 const OkkamiSdkManager = NativeModules.OkkamiSdk
@@ -293,7 +294,12 @@ class OkkamiSdk {
    */
   async getBatteryLevel () {
     try {
-      return await OkkamiSdkManager.getBatteryLevel()
+      if (Metrics.platform === 'android') {
+        let batt = await OkkamiSdkManager.getBatteryLevel()
+        return batt.bLevel
+      } else {
+        return await OkkamiSdkManager.getBatteryLevel()
+      }
     } catch (e) {
       return e
     }
@@ -304,7 +310,12 @@ class OkkamiSdk {
    */
   async getUptimeMillis () {
     try {
-      return await moment().subtract(OkkamiSdkManager.getUptimeMillis(), 'ms').fromNow(true)
+      if (Metrics.platform === 'android') {
+        let uptime = await OkkamiSdkManager.getUptimeMillis()
+        return moment().subtract(uptime.uptime, 'ms').fromNow(true)
+      } else {
+        return await moment().subtract(OkkamiSdkManager.getUptimeMillis(), 'ms').fromNow(true)
+      }
     } catch (e) {
       return e
     }
@@ -315,7 +326,12 @@ class OkkamiSdk {
    */
   async getWifiSignalStrength () {
     try {
-      return await OkkamiSdkManager.getWifiSignalStrength()
+      if (Metrics.platform === 'android') {
+        let s = await OkkamiSdkManager.getWifiSignalStrength()
+        return s.strength
+      } else {
+        return await OkkamiSdkManager.getWifiSignalStrength()
+      }
     } catch (e) {
       return e
     }
@@ -326,7 +342,12 @@ class OkkamiSdk {
    */
   async getWifiSSID () {
     try {
-      return await OkkamiSdkManager.getWifiSSID()
+      if (Metrics.platform === 'android') {
+        let ssid = await OkkamiSdkManager.getWifiSSID()
+        return  ssid.ssid
+      } else {
+        return await OkkamiSdkManager.getWifiSSID()
+      }
     } catch (e) {
       return e
     }
@@ -337,7 +358,12 @@ class OkkamiSdk {
    */
   async getIPv4 () {
     try {
-      return await OkkamiSdkManager.getIPv4()
+      if (Metrics.platform === 'android') {
+        let ipv4 = await OkkamiSdkManager.getIPv4()
+        return ipv4.ipv4
+      } else {
+        return await OkkamiSdkManager.getIPv4()
+      }
     } catch (e) {
       return e
     }
@@ -348,7 +374,12 @@ class OkkamiSdk {
    */
   async getIPv6 () {
     try {
-      return await OkkamiSdkManager.getIPv6()
+      if (Metrics.platform === 'android') {
+        let ipv6 = await OkkamiSdkManager.getIPv6()
+        return ipv6.ipv6
+      } else {
+        return await OkkamiSdkManager.getIPv6()
+      }
     } catch (e) {
       return e
     }
@@ -359,7 +390,12 @@ class OkkamiSdk {
    */
   async getWifiMac () {
     try {
-      return await OkkamiSdkManager.getWifiMac()
+      if (Metrics.platform === 'android') {
+        let mac = await OkkamiSdkManager.getWifiMac()
+        return mac.mac
+      } else {
+        return await OkkamiSdkManager.getWifiMac()
+      }
     } catch (e) {
       return e
     }
