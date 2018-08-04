9245384c75f7f30391106e8c0c1060e1c686aa18
Shrouk Khan <shroukkhan@gmail.com>
2018-08-04 23:30:09 +0700

    FD-6009 : Thin Frameworks - only okkami dev as test

--- ios/RCTOkkamiSdk/RCTOkkamiSdk.m
+++ ios/RCTOkkamiSdk/RCTOkkamiSdk.m
@@ -5,8 +5,10 @@
 #include <ifaddrs.h>
 #include <arpa/inet.h>
 #include <net/if.h>
+#if TARGET_OS_SIMULATOR
+#else
 #import <OpenKeySDK/OpenKeySDK.h>
-
+#endif
 #define IOS_CELLULAR    @"pdp_ip0"
 #define IOS_WIFI        @"en0"
 #define IOS_VPN       @"utun0"
